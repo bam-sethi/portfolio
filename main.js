92fb68758a34b85516ee4033f342f8fa27d3d454
Ambar Sethi <ambarrrsethi@gmail.com>
2015-11-10 10:48:00 +0000

    adding jQuery and changing page style

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("jake weary")
+
+$(document).ready(function(){
+
+  $("#contact-toggle").on("click", function(){
+    console.log("togglee mee")
+    $("#contact-container").toggle();
+  })
+
+
+})
+
